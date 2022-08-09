ff1359d180731436a7f6e9db548e55d6b6ed00f0
Aakash Lokhande <aakash.slokhande.mst17@itbhu.ac.in>
2022-08-09 16:11:15 +0530

    commit2

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello Swiggy ,From Jenkins!! <1")
+		return c.HTML(http.StatusOK, "Hello Aakash ,From Jenkins!! <1")
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
